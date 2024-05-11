daecada74cdeb3dbfc7f62e424239d1876196aba
NisargaBasavarajuu <145655608+NisargaBasavarajuu@users.noreply.github.com>
2024-05-11 16:20:54 +0530

    Update models.py

--- calculator/models.py
+++ calculator/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 class Calculator(models.Model):
-    input = models.CharField(max_length=255)
+    input_data = models.CharField(max_length=255)
     result = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        # Add custom validation logic if needed
+        super().save(*args, **kwargs)
 

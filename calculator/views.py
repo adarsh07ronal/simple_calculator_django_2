62c40999c6abcc24179023e5094c4d199fee7905
adarsh07ronal <adarsh07ronal@gmail.com>
2024-05-19 10:45:22 +0530

    test

--- calculator/views.py
+++ calculator/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.http import HttpResponse
 from .models import Result
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def calculate(request):
     try:
         expression = request.GET.get('expression', '')
@@ -11,14 +13,15 @@ def calculate(request):
         return JsonResponse({"result": result})
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=400)
-    
+
+@csrf_exempt   
 def home(request):
     return JsonResponse({"message": "Welcome to the Calculator App!"})
-
+@csrf_exempt
 def add(request):
     return HttpResponse("CALCULATOR")
 
-
+@csrf_exempt
 def calculate_and_store(request):
     if request.method == "POST":
         expression = request.POST.get('expression')
@@ -32,6 +35,7 @@ def calculate_and_store(request):
 
     return render(request, 'calculate.html')
 
+@csrf_exempt
 def display_results(request):
     results = Result.objects.all()
     return render(request, 'results.html', {'results': results})
